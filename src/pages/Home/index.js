774efe4b0e3823ed0f8777a34cde1c40463494f7
EmersonOliveiraDev <emersonhaw@gmail.com>
2020-05-17 04:16:50 -0300

    Add products grid

---
+++ src/pages/Home/index.js
@@ -0,0 +1,97 @@
+import React from 'react'
+import { MdShoppingCart } from 'react-icons/md'
+import { ProductList } from './styles'
+
+export default function Home() {
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-oakley-split-masculino/06/D63-5814-006/D63-5814-006_zoom2.jpg?ts=1560768246&ims=326x"
+          alt="Tênis"
+        />
+        <strong>
+          Tênis sdfsdf s sd fsdf sd f sdf s dfsd fsd fsdf sdf sdf dsfsd fsd fsdf
+          sdf sd fsd fsd fsd
+        </strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-oakley-split-masculino/06/D63-5814-006/D63-5814-006_zoom2.jpg?ts=1560768246&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-oakley-split-masculino/06/D63-5814-006/D63-5814-006_zoom2.jpg?ts=1560768246&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-oakley-split-masculino/06/D63-5814-006/D63-5814-006_zoom2.jpg?ts=1560768246&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-oakley-split-masculino/06/D63-5814-006/D63-5814-006_zoom2.jpg?ts=1560768246&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-oakley-split-masculino/06/D63-5814-006/D63-5814-006_zoom2.jpg?ts=1560768246&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+    </ProductList>
+  )
+}
