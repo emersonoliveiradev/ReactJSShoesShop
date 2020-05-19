68896fce6312c30038ecfbcc5cf0b60987898a5c
EmersonOliveiraDev <emersonhaw@gmail.com>
2020-05-19 00:53:18 -0300

    Config base reducer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { provider } from 'react-redux'
 
 import GlobalStyle from './styles/global'
 import Header from './components/Header'
 import Routes from './routes'
 
+import store from './store'
+
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes />
-      <GlobalStyle />
-    </BrowserRouter>
+    <provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes />
+        <GlobalStyle />
+      </BrowserRouter>
+    </provider>
   )
 }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,97 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { MdShoppingCart } from 'react-icons/md'
+import { formatPrice } from '../../util/format'
+import api from '../../services/api'
 import { ProductList } from './styles'
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-oakley-split-masculino/06/D63-5814-006/D63-5814-006_zoom2.jpg?ts=1560768246&ims=326x"
-          alt="Tênis"
-        />
-        <strong>
-          Tênis sdfsdf s sd fsdf sd f sdf s dfsd fsd fsdf sdf sdf dsfsd fsd fsdf
-          sdf sd fsd fsd fsd
-        </strong>
-        <span>R$ 129,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart />
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-oakley-split-masculino/06/D63-5814-006/D63-5814-006_zoom2.jpg?ts=1560768246&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis</strong>
-        <span>R$ 129,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart />
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-oakley-split-masculino/06/D63-5814-006/D63-5814-006_zoom2.jpg?ts=1560768246&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis</strong>
-        <span>R$ 129,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart />
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-oakley-split-masculino/06/D63-5814-006/D63-5814-006_zoom2.jpg?ts=1560768246&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis</strong>
-        <span>R$ 129,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart />
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-oakley-split-masculino/06/D63-5814-006/D63-5814-006_zoom2.jpg?ts=1560768246&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis</strong>
-        <span>R$ 129,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart />
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-oakley-split-masculino/06/D63-5814-006/D63-5814-006_zoom2.jpg?ts=1560768246&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis</strong>
-        <span>R$ 129,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart />
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-    </ProductList>
-  )
+export default class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      products: [],
+    }
+  }
+
+  async componentDidMount() {
+    const response = await api.get('products')
+
+    const data = response.data.map((product) => ({
+      ...product,
+      priceFormated: formatPrice(product.price),
+    }))
+
+    this.setState({
+      products: data,
+    })
+  }
+
+  render() {
+    const { products } = this.state
+
+    return (
+      <ProductList>
+        {products.map((product) => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.strong}</strong>
+            <span>{product.priceFormated}</span>
+            <button type="button">
+              <div>
+                <MdShoppingCart /> 45
+              </div>
+              <span>Adicionar ao carrinho</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    )
+  }
 }
